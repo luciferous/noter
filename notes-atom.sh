c9ac23dd995ae1b5c3a5c141bd07307d87fb4a7a
Neuman Vong <neuman.vong@gmail.com>
2017-03-15 01:24:11 -0700

    atom: site_url and prefix

--- notes-atom.sh
+++ notes-atom.sh
@@ -3,7 +3,7 @@
 cat <<EOF
 <?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
-  <id>$full_prefix</id>
+  <id>$site_url$prefix</id>
   <title>$site_title</title>
   <updated>$now</updated>
 EOF
@@ -11,7 +11,7 @@ EOF
 while IFS=$'\t' read -r FILE TITLE UPDATED; do
   cat <<EOF
   <entry>
-    <id>${full_prefix}$FILE</id>
+    <id>$site_url$prefix$FILE</id>
     <title>$TITLE</title>
     <updated>$UPDATED</updated>
   </entry>
